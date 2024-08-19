7b259c1ea091e518bf046bce45a820815585846c
Md. Mobashirul Alam Seam <mobashirulalamseam@gmail.com>
2024-08-19 12:25:26 +0600

    Basic navbar and banner created

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from "@/providers/theme-provider";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import type { Metadata } from "next";
@@ -18,8 +19,17 @@ export default function RootLayout({
 }>) {
     return (
         <ClerkProvider appearance={{ baseTheme: dark }}>
-            <html lang="en">
-                <body className={inter.className}>{children}</body>
+            <html lang="en" suppressHydrationWarning>
+                <body className={inter.className}>
+                    <ThemeProvider
+                        attribute="class"
+                        defaultTheme="system"
+                        enableSystem
+                        disableTransitionOnChange
+                    >
+                        {children}
+                    </ThemeProvider>
+                </body>
             </html>
         </ClerkProvider>
     );
